01a10eedcf3ce345bb2db76f27063977efe46169
LGugs <lgpa@icomp.ufam.edu.br>
2022-06-10 15:11:42 -0400

    making game page

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -8,11 +8,34 @@ const GameScreen = ({ check, score, categoryWord }) => {
   console.log(categoryWord);
 
   return (
-    <div>
-        <p>Pontuação: {score}</p>
+    <div className="game">
+        <p className="points">Pontuação: {score}</p>
         <h2>Advinhe a palavra:</h2>
-        <h3>Dica sobre a palavra: {categoryWord}</h3>
+        <h3 className="tip">
+          Dica sobre a palavra: <span> {categoryWord} </span>
+        </h3>
+
+        <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blankSquare"></span>
+        </div>
+
+        <div className="letterContainer">
+          <p>Tente advinhar uma letra da palavra</p>
+          <form>
+            <input type="text" name="letter" maxLength="1" required  />
+            <button>Jogar!</button>
+          </form>
+        </div>
+
+        <div className="wrongLettersContainer">
+          <p>Letras já utilizadas:</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
+
         <p>Você ainda tem {tryNum} tentativa(s).</p>
+
         <button onClick={check}>Finalizar Jogo!</button>
     </div>
   )
